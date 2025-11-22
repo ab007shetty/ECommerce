37bd5acc0103c35c4456d55e2e4bc39483a72a0c
Anirudha B Shetty <ab007shetty@email.com>
2025-11-22 21:55:15 +0530

    Removed build.js

--- build.js
+++
@@ -1,4 +0,0 @@
-// build.js  ← this file makes Vercel happy
-const { execSync } = require("child_process");
-console.log("Building frontend...");
-execSync("npm install && npm run build", { cwd: "Frontend", stdio: "inherit" });
